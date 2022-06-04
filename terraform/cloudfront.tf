f4e66b00b5992f22c4bac3b17f91a9b95e4635c1
jrgermain <37086292+jrgermain@users.noreply.github.com>
2022-06-04 15:13:28 -0400

    Try to fix error yet again

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -21,7 +21,7 @@ resource "aws_cloudfront_distribution" "frontend" {
 
   origin {
     origin_id   = local.origin_id_api
-    domain_name = substr(aws_apigatewayv2_stage.default.invoke_url, 8, -2)
+    domain_name = trimsuffix(trimprefix(aws_apigatewayv2_stage.default.invoke_url, "https://"), "/")
 
     custom_origin_config {
       http_port              = 80
